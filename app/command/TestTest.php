c513e8a7127c3c89b1ddb2b0597f3564cf139361
ethan927 <chengguangxin927@gmail.com>
2023-10-26 10:08:25 +0800

    Update TestTest.php
    fix

--- app/command/TestTest.php
+++ app/command/TestTest.php
@@ -74,7 +74,7 @@ class TestTest extends Command
                 'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
                 'cookie' => '__ac_nonce=0638733a400869171be51',
-            ]
+            ],
             'verify' => false
         ]);
     }
